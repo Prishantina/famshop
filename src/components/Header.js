e22184080509ed48f378fc04e8819bb8b8f03609
Prishantina <prishantina@gmail.com>
2022-03-28 13:16:42 -0300

    agrego navbar

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import Logo from '../imagenes/Logo-F.png';
+const Header = () => {
+    return(
+        <header className = "navegacion">
+            <img src={Logo} alt="logo"/>
+        </header>
+    )
+};
+
+export default Header;
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+    return(
+        <nav>
+            <ul className ="Navegacion">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    )
+};
+
+export default NavBar;
